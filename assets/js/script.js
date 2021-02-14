f301347b17eecd6e5d639043e51ee837ac17e80d
David Eldridge <d.vincenteldridge@gmail.com>
2021-02-14 14:37:12 -0500

    got local storage to save, date to show, and classes to change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,49 @@
-var rootEl = $('#task');
+var rootEl = document.querySelectorAll('.task');
 console.log(rootEl);
+var timeBlockEl = document.querySelectorAll(".row");
+console.log(timeBlockEl);
 // let task;
 
 
 // let valueArray = [];
 $(document).ready(function () {
-   
+    var dateTime = moment().format("MMM DD YYYY");
+    $("#currentDay").text(dateTime);
+    console.log(dateTime);
+
     //check for button clicks inside an on click
     $(".saveBtn").on("click", function () {
         //grab my values for time and text
         var task = $(this).siblings(".task").val();
         var time = $(this).parent().attr("id");
-        // console.log(task, time);
+        
         //save them to localStorage
-        localStorage.setItem('time', JSON.stringify(time));
-        localStorage.setItem('task', JSON.stringify(task));  
+        localStorage.setItem(time, JSON.stringify(task));    
     });
    
-    //write a function to update the hours in the time blocks
-    let timeArray = [];
-    timeArray.push(JSON.parse(localStorage.getItem('time')));
-    console.log(timeArray);
-    // //get current time use moment
-    var currentTime = moment().format("hh");
-    $("#4a").text(currentTime);
+    var currentTime = moment().format("HH");
     console.log(currentTime);
-  
 
     // //loop over our time blocks hint (look up .each)
-    for (let i = 0; i < timeArray.length; i++) {
-        let hourNumber = timeArray[i].split('-');
-        var taskTime = hourNumber[1];
-        console.log(taskTime);
-        // //check to see if we have moved past the current time
-        // //if
-        // //addClass
-        // //addClass("past")
-       
-        if (taskTime < currentTime) {
-            console.log(time);
-            rootEl.addClass('bg-secondary', 'past');
+    for (let i = 0; i < timeBlockEl.length; i++) {
+        let hourNumber = timeBlockEl[i].id; 
+        console.log(hourNumber);
+        localStorage.getItem(hourNumber);
+        console.log(hourNumber);
+        rootEl[i].value = localStorage.getItem(hourNumber);
+        
+        let idHour = hourNumber.split('-');
+        let singleHour = Number(idHour[1]);
+        console.log(singleHour);
+
+        if (singleHour < currentTime) {
+            rootEl[i].classList.add('past');
+        } else if (singleHour == currentTime) {
+            rootEl[i].classList.add('present');
+        } else if (singleHour > currentTime) {
+            rootEl[i].classList.add('future');
         }
-    
-
-    // //check to see if it is the current time
-    // //else if
-    // //removeClass
-    // //addClass
-
-    // //check to see if time is future
-    // //else
-    // //removeClass
-    // //removeClass
-    // //addClass
-    // //});
-    // //load any saved data from localStorage
-
-    // // $("#hour-9 .task").val(localStorage.getItem.length("hour-9"));
     }
-    // //display current day on page (using moment)
+
 });
 
-// //activity 04-26 global array 
-// // 
